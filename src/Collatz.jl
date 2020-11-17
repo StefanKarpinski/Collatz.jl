11878894a9312d42a2c910bf9a8d349bd6e025cc
Stefan Karpinski <stefan@karpinski.org>
2020-11-17 15:29:55 -0500

    collatz function: define, export and test

--- src/Collatz.jl
+++ src/Collatz.jl
@@ -1,5 +1,16 @@
 module Collatz
 
-# Write your package code here.
+using Base.Checked
+
+export collatz
+
+function collatz(n::Integer)
+    k = 0
+    while n > 1
+        n = iseven(n) ? n >> 1 : checked_add(checked_mul(3,n), 1)
+        k += 1
+    end
+    return k
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,23 @@ using Collatz
 using Test
 
 @testset "Collatz.jl" begin
-    # Write your tests here.
+    @test collatz(0) == 0
+    @test collatz(1) == 0
+    @test collatz(9) == 19
+    @test collatz(97) == 118
+    @test collatz(871) == 178
+    @test collatz(6171) == 261
+    @test collatz(77031) == 350
+    @test collatz(837799) == 524
+    @test collatz(8400511) == 685
+    @test collatz(63728127) == 949
+    @test collatz(670617279) == 986
+    @test collatz(9780657630) == 1132
+    @test collatz(75128138247) == 1228
+    @test collatz(989345275647) == 1348
+    @test collatz(Int128(7887663552367)) == 1563
+    @test collatz(80867137596217) == 1662
+    @test collatz(942488749153153) == 1862
+    @test collatz(Int128(7579309213675935)) == 1958
+    @test collatz(Int128(93571393692802302)) == 2091
 end
